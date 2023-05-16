2fe72c9c9c4e0fc73dcff13c0c59601f7e8f7ea7
Mike Fortunato <103296852+ofk7@users.noreply.github.com>
2023-05-16 05:12:10 -0300

    Update script.js

--- script.js
+++ script.js
@@ -11,7 +11,7 @@ const loop = setInterval(() => {
         mario.style.animation = 'none';
         mario.style.bottom = `${marioPosition}px`;
 
-        mario.src = './Images/game-over.png';
+        mario.src = 'https://ofk7.github.io/learningalone/game-over.png';
         mario.style.width = '75px'
         mario.style.marginLeft = '50px'
     }
